b725d75e0245a09a1017a523f4d62e4909ac4488
Mario Tomelin <tomelin5@gmail.com>
2019-12-03 21:19:35 -0300

    3 parte2

--- 3.py
+++ 3.py
@@ -66,25 +66,39 @@ for point in cable2:
 plt.plot(xgraph, ygraph, label="line2")
 
 #buscar cruces
+steps1 = steps2 = 0
 
-cruces = []
 for a in range(0, len(cable1)-1):
 	x1,y1 = cable1[a]
 	x2,y2 = cable1[a+1]
 	#print(a)
+	steps1 += abs(x2-x1) #acumulo steps1
+	steps1 += abs(y2-y1) #acumulo steps1
 
 	for b in range(0, len(cable2)-1):
 		x3,y3 = cable2[b]
 		x4,y4 = cable2[b+1]
+		steps2 += abs(x4-x3) #acumulo steps2
+		steps2 += abs(y4-y3) #acumulo steps2
+
 
 		# cable2 vertical, cable1 horizontal | asumimos que se cruzan uno vertical y otro H
 		#  c2vertila                y   está posicionado en el rango-X y lo cruza en Y
 		if(x4-x3==0 and y1-y2 == 0 and (min(x1,x2) < x3 < max(x1,x2)) and (min(y3,y4) < y2 < max(y3,y4))):
-			print("bingo 1:",x3,y1,"distance:",abs(x3)+abs(y1))
-
+			parcial1 = steps1 - abs(x2-x1) + abs(x1-x3) #resta actual y suma parcial
+			parcial2 = steps2 - abs(y4-y3) + abs(y1-y3)
+			steps_total = parcial1+parcial2
+			print("bingo 1:",x3,y1,"distance:",abs(x3)+abs(y1), "steps_total",steps_total,parcial1,parcial2)
+		
 		# c2 horizonal
 		if(x1-x2==0 and y3-y4 == 0 and (min(x3,x4) < x1 < max(x3,x4)) and (min(y1,y2) < y3 < max(y1,y2))):
-			print("bingo 2:",x1,y3, "distance:",abs(x1)+abs(y3))
+			parcial1 = steps1 - abs(y1-y2) + abs(y1-y3) #resta actual y suma parcial
+			parcial2 = steps2 - abs(x4-x3) + abs(x1-x3)
+			steps_total = parcial1+parcial2
+			print("bingo 2:",x1,y3, "distance:",abs(x1)+abs(y3), "steps_total",steps_total,parcial1,parcial2)
+	
+	#end for, reset cable
+	steps2 = 0
 
 
 
